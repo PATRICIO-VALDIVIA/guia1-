315d993ed0912c9470b0b580a91ae8995ce4f6ca
PATRICIO-VALDIVIA <63594688+PATRICIO-VALDIVIA@users.noreply.github.com>
2020-04-15 21:48:24 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/estilo.css" rel="stylesheet" type="text/css"/>
+        <title>Aperture science spa</title>
+    </head>
+        
+    <body>
+        <header id="encabezado">
+         <hgroup>
+                <h1>Pistolas de portales</h1>
+      
+        </hgroup>
+            
+             
+            <nav>
+                <ul>
+                    <li>Informacion </li>
+                    <li>Contacto</li>
+                    <li>¿Quienes somos?</li>
+                    
+                </ul>
+            </nav>
+            
+        </header>
+        
+        <section>
+            <div id="Introducion">    
+         <p> El Dispositivo Portátil de Portales de Aperture Science, 
+                    también conocido comúnmente como Pistola de Portales o por sus siglas en inglés,
+                    "ASHPD" (Aperture Science Handheld Portal Device), 
+                    es una herramienta experimental utilizada para crear dos portales a través de la cual los objetos pueden pasar. 
+                    Se utiliza en las cámaras de prueba del Centro de Desarrollo de Aperture Science, 
+                    es la herramienta principal que el Sujeto de Prueba debe utilizar para completar cada prueba.
+         </p>
+         
+         <p>La Pistola de Portales es un dispositivo que tiene la capacidad de fabricar dos portales enlazados. 
+             No importa la distancia entre ellos, cualquier objeto que pase a través de un portal saldrá del otro y
+             viceversa instantáneamente. Los portales se pueden colocar en cualquier superficie que sea manufacturada o 
+             refinada con roca lunar y sea lo suficientemente grande para acomodarse a ella. Objetos, 
+             o ciertos tipos de superficies en movimiento, generalmente previenen la formación de un portal.
+             La Red de Emancipación Material de Aperture Science bloqueará cualquier intento de disparar portales a través 
+             de ella, así como desactivara los portales activos si el ASHPD pasa a través de élla.
+             
+         </p>
+         
+         <p>El ASHPD se sostiene con una mano por detrás del arma, sosteniendo el mango.
+             La luz en el pequeño agujero en la parte superior del ASHPD y su tubo de vidrio brillante tiene el color
+             del último portal disparado. Tiene dos disparadores, uno para cada uno de sus dos portales.
+             
+         </p>
+         
+         <aside>
+         <p>Cuando se dispara el ASHPD, una ráfaga de energía de color se emite desde el cañón, correspondiente al portal
+             del color que se pretende crear. Al golpear la superficie objetivo, se forma un portal, rodeado por un anillo
+             del color respectivo. Si la superficie no puede dar cabida a un portal por cualquier razón, el tiro se 
+             disipará en una lluvia de partículas del color respectivo. El dispositivo sólo puede crear dos portales a la vez.
+             Si un tercero se dispara, el otro de su tipo se cerrará automáticamente y se formará un nuevo enlace.
+             
+         </p>
+         </aside>
+            </div>
+                
+        </section>
+        
+        <img src="resources/1366_2000.jpg" width="320" lang="320" />    
+        
+         
+       
+    </body>
+     <footer>
+        <p>Numero: +5695587267</p>
+        <p>Direccion: Calle Falsa 123</p>
+        <p>Correo: correofalso123@gmail.com</p>
+    </footer>
+</html>
